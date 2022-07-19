e8d95add0d30bb48791821410dcaf6eb02d06db3
hmiwa30 <109275358+hmiwa30@users.noreply.github.com>
2022-07-19 22:02:24 +0900

    Update yanarhs_bzip.sh
    [change]
    if [ $SGE_TASK_ID -lt 10 ]; then
        bzips /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhsf3/yana_merged${SGE_TASK_ID}.txt;echo "done yana_j_merged0${SGE_TASK_ID}.txt"
        bzip2 /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhs_jomonf3/yana_j_merged${SGE_TASK_ID}.txt;echo "done yana_j_merged0${SGE_TASK_ID}.txt"
    
    echo "start: `date` ${SGE_TASK_ID} "
    echo "finish: `date` ${SGE_TASK_ID} "

--- yanarhs_bzip.sh
+++ yanarhs_bzip.sh
@@ -10,12 +10,12 @@
 #$ -o ~/hmiwa/log/yanarhs_bzip.o
 #$ -e ~/hmiwa/log/yanarhs_bzip.e
 #$ -t 1-22:1
-echo "start: `date` ${SGE_TASK_ID} "
+echo "start: `date` chr ${SGE_TASK_ID} "
 if [ $SGE_TASK_ID -lt 10 ]; then
-    bzips /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhsf3/yana_merged${SGE_TASK_ID}.txt;echo "done yana_j_merged0${SGE_TASK_ID}.txt"
-    bzip2 /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhs_jomonf3/yana_j_merged${SGE_TASK_ID}.txt;echo "done yana_j_merged0${SGE_TASK_ID}.txt"
+    bzip2 /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhsf3/yana_merged0${SGE_TASK_ID}.txt;echo "done yana_j_merged0${SGE_TASK_ID}.txt"
+    bzip2 /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhs_jomonf3/yana_j_merged0${SGE_TASK_ID}.txt;echo "done yana_j_merged0${SGE_TASK_ID}.txt"
 else
-    bzips /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhsf3/yana_merged${SGE_TASK_ID}.txt;echo "done yana_j_merged${SGE_TASK_ID}.txt"
+    bzip2 /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhsf3/yana_merged${SGE_TASK_ID}.txt;echo "done yana_j_merged${SGE_TASK_ID}.txt"
     bzip2 /lustre7/home/hmiwa-spc/hmiwa/analysis_yanarhsf3/yanarhs_jomonf3/yana_j_merged${SGE_TASK_ID}.txt;echo "done yana_j_merged${SGE_TASK_ID}.txt"
 fi
-echo "finish: `date` ${SGE_TASK_ID} "
+echo "finish: `date` chr ${SGE_TASK_ID} "

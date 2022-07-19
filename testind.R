e48b6b39a99a3c70527d47ed04e5ae3f1afb83ab
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-07-19 19:39:17 +0900

    [add] git add -A *.R

---
+++ testind.R
@@ -0,0 +1,14 @@
+pops <- read.csv("downloads/1KG/1000G.p3.name.csv",header=F)
+pop_yana <- read.table("analysis_yanarhsf3/samplelist.txt",header=F,sep=",")
+
+test_sum <- as.vector(pops$V1[-c(1)])
+#test_sum <- append(test_sum,c("Jomon","IK002.maf001_Jomon","JpFu1","JpKo13","JpHi01","JpKo2","JpIw31","JpOd181","JpIw32","JpOd274","JpIw33","JpOd282","JpKa6904","JpOd6"))
+test_sum <- append(test_sum,as.character(pop_yana$V1))
+test_pop <- as.vector(pops$V7[-c(1)])
+#test_pop <- append(test_pop,c("Jomon","IK002.maf001_Jomon","JpFu1","JpKo13","JpHi01","JpKo2","JpIw31","JpOd181","JpIw32","JpOd274","JpIw33","JpOd282","JpKa6904","JpOd6"))
+test_pop <- append(test_pop,as.character(pop_yana$V2))
+df1 <- data.frame(v1=test_sum,v2="U",v3=test_pop)
+
+write.table(df1,"analysis_yanarhsf3/test.ind",quote=F,col.names=F,row.names=F)
+write.table(df1,"analysis_yanarhsf3/yanarhsf3/test.ind",quote=F,col.names=F,row.names=F)
+write.table(df1,"analysis_yanarhsf3/yanarhs_jomonf3/test.ind",quote=F,col.names=F,row.names=F)

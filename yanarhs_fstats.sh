c62510d970cae70737ce86980cd60cd9e902f716
hmiwa30 <109275358+hmiwa30@users.noreply.github.com>
2022-07-19 20:57:10 +0900

    Update yanarhs_fstats.sh
    [delete]  ,[3] ->  [3]

--- yanarhs_fstats.sh
+++ yanarhs_fstats.sh
@@ -9,10 +9,10 @@
 #$ -o ~/hmiwa/log/yanarhs_fstats.o
 #$ -e ~/hmiwa/log/yanarhs_fstats.e
 #$ -t 1-4:1
-tag_names=([1]="_yanarhs" [2]="_yanarhsjomon",[3]="_yanarhs" [4]="_yanarhsjomon")
+tag_names=([1]="_yanarhs" [2]="_yanarhsjomon" [3]="_yanarhs" [4]="_yanarhsjomon")
 tag_name=${tag_names[$SGE_TASK_ID]}
-file_names1=([1]="parqpfstat" [2]="parqpfstat",[3]="parqpfstat2" [4]="parqpfstat2")
-file_names2=([1]="qpfstatlog" [2]="qpfstatlog",[3]="qpfstatlog2" [4]="qpfstatlog2")
+file_names1=([1]="parqpfstat" [2]="parqpfstat" [3]="parqpfstat2" [4]="parqpfstat2")
+file_names2=([1]="qpfstatlog" [2]="qpfstatlog" [3]="qpfstatlog2" [4]="qpfstatlog2")
 file_name1=${file_names1[$SGE_TASK_ID]}
 file_name2=${file_names2[$SGE_TASK_ID]}
 #dir_names=([1]="yanarhsf3" [2]="yanarhs_jomonf3")

e9ac108d81e42ef20a796f63c43da2eab1e48431
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-01 22:54:36 +0900

    Create calculate_f3_0801.R

---
+++ localRfstats/calculate_f3_0801.R
@@ -0,0 +1,48 @@
+.libPaths()
+.libPaths("C:\\Users\\himar\\Documents\\R")
+
+#install.packages("shiny")
+#install.packages("shinythemes")
+#install.packages("shinyjs")
+#install.packages("shinyBS")
+#install.packages("shinyFiles")
+#install.packages("shinyalert")
+#install.packages("shinyWidgets")
+#install.packages("shinydashboard")
+#install.packages("DT")
+#install.packages("jquerylib")
+library(admixtools)
+
+#denisova
+sdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\sources"
+rdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\results"
+ldir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3"
+
+(prefix = sprintf("%s\\yana_j_f3_analysis_denisova",sdir))
+(my_f2_dir = sprintf("%s\\yana_j_f3_analysis_denisova",rdir))
+extract_f2(prefix, my_f2_dir)
+
+f2_blocks = f2_from_precomp(my_f2_dir)
+dim(f2_blocks)
+(lista <- read.table(sprintf("%s\\lista_yanajomon.txt",ldir))$V1)
+(pop1 = as.character(lista[1]))
+(pop2 = as.character(lista[9]))
+(pop3 = as.character(lista[-1]))
+dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+write.table(dat.f3,sprintf("%s\\yana_j_f3_analysis_denisova.f3_FIN.txt",rdir),quote=F,row.names=F)
+
+#for(ah in c("altai","chagyrskaya","denisova","vindija")){
+#  prefix = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\Rename\\j_f3_analysis_%s",ah)
+#  my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\output_%s",ah)
+#  extract_f2(prefix, my_f2_dir)
+#  f2_blocks = f2_from_precomp(my_f2_dir)
+#  dim(f2_blocks)
+#  lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
+#  (pop1 = as.character(lista[1]))
+#  (pop2 = as.character(lista[9]))
+#  (pop3 = as.character(lista[-1]))
+#  dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+#  write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\f3_FIN_%s.txt",ah),
+#              quote=F,row.names=F)
+#}
+

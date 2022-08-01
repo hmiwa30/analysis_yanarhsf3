fb46eb80c9f01e1e1ac52eda22ec243374d27db4
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-01 22:59:31 +0900

    Update calculate_f3_0801.R

--- localRfstats/calculate_f3_0801.R
+++ localRfstats/calculate_f3_0801.R
@@ -13,23 +13,68 @@
 #install.packages("jquerylib")
 library(admixtools)
 
-#denisova
+gc(reset=T)
+gc(reset=T)
+
 sdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\sources"
 rdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\results"
 ldir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3"
 
-(prefix = sprintf("%s\\yana_j_f3_analysis_denisova",sdir))
-(my_f2_dir = sprintf("%s\\yana_j_f3_analysis_denisova",rdir))
-extract_f2(prefix, my_f2_dir)
+for(ah in c("altai","chagyrskaya","denisova","vindija")){
+  
+  (prefix = sprintf("%s\\yana_j_f3_analysis_%s",sdir,ah))
+  (my_f2_dir = sprintf("%s\\yana_j_f3_analysis_%s",rdir,ah))
+  extract_f2(prefix, my_f2_dir)
+  
+  f2_blocks = f2_from_precomp(my_f2_dir)
+  dim(f2_blocks)
+  (lista <- read.table(sprintf("%s\\lista_yanajomon.txt",ldir))$V1)
+  (pop1 = as.character(lista[1]))
+  (pop2 = as.character(lista[9]))
+  (pop3 = as.character(lista[-1]))
+  dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+  write.table(dat.f3,sprintf("%s\\yana_j_f3_analysis_%s.f3_FIN.txt",rdir,ah),quote=F,row.names=F)
+  
+  gc(reset=T)
+  gc(reset=T)
+  
+}
 
-f2_blocks = f2_from_precomp(my_f2_dir)
-dim(f2_blocks)
-(lista <- read.table(sprintf("%s\\lista_yanajomon.txt",ldir))$V1)
-(pop1 = as.character(lista[1]))
-(pop2 = as.character(lista[9]))
-(pop3 = as.character(lista[-1]))
-dat.f3=f3(f2_blocks,pop1,pop2,pop3)
-write.table(dat.f3,sprintf("%s\\yana_j_f3_analysis_denisova.f3_FIN.txt",rdir),quote=F,row.names=F)
+for(ah in c("altai","chagyrskaya","denisova","vindija")){
+  
+  (prefix = sprintf("%s\\yana_j_f3_analysis2_%s",sdir,ah))
+  (my_f2_dir = sprintf("%s\\yana_j_f3_analysis2_%s",rdir,ah))
+  extract_f2(prefix, my_f2_dir)
+  
+  f2_blocks = f2_from_precomp(my_f2_dir)
+  dim(f2_blocks)
+  (lista <- read.table(sprintf("%s\\lista2_yanajomon.txt",ldir))$V1)
+  (pop1 = as.character(lista[1]))
+  (pop2 = as.character(lista[9]))
+  (pop3 = as.character(lista[-1]))
+  dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+  write.table(dat.f3,sprintf("%s\\yana_j_f3_analysis2_%s.f3_FIN.txt",rdir,ah),quote=F,row.names=F)
+  
+  gc(reset=T)
+  gc(reset=T)
+  
+}
+
+
+
+
+#(prefix = sprintf("%s\\yana_j_f3_analysis_denisova",sdir))
+#(my_f2_dir = sprintf("%s\\yana_j_f3_analysis_denisova",rdir))
+#extract_f2(prefix, my_f2_dir)
+
+#f2_blocks = f2_from_precomp(my_f2_dir)
+#dim(f2_blocks)
+#(lista <- read.table(sprintf("%s\\lista_yanajomon.txt",ldir))$V1)
+#(pop1 = as.character(lista[1]))
+#(pop2 = as.character(lista[9]))
+#(pop3 = as.character(lista[-1]))
+#dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+#write.table(dat.f3,sprintf("%s\\yana_j_f3_analysis_denisova.f3_FIN.txt",rdir),quote=F,row.names=F)
 
 #for(ah in c("altai","chagyrskaya","denisova","vindija")){
 #  prefix = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\Rename\\j_f3_analysis_%s",ah)

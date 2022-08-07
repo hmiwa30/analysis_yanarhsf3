c47e4c460e9b9d9a12e7e1f277807f7fc5ba961d
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-08 01:48:04 +0900

    Create f3_plot_0808.R

---
+++ localRfstats/f3_plot_0808.R
@@ -0,0 +1,4 @@
+sdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\sources"
+rdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\results"
+ldir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3"
+

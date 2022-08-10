fac48f96dfe446d1b7d09223a925f4ab80c8cb5c
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-10 16:49:30 +0900

    [add] split_geno_0810.sh

---
+++ localRfstats/split_geno_0810.sh
@@ -0,0 +1,12 @@
+#!/mnt/hdd/hmiwa
+echo "start:`date`"
+#yana_j_f3_analysis
+part1=$(< yana_j_f3_analysis_part1.txt)
+part2=$(< yana_j_f3_analysis_part2.txt)
+part3=$(< yana_j_f3_analysis_part3.txt)
+part4=$(< yana_j_f3_analysis_part4.txt)
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis.geno | cut -b $part1 > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part1.geno
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis.geno | cut -b $part2 > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part2.geno
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis.geno | cut -b $part3 > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part3.geno
+cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis.geno | cut -b $part4 > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part4.geno
+echo "finish:`date`"

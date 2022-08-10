bd5af396c27d238e4efab2429106386eac03f935
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-10 17:54:35 +0900

    [update] split_geno_0810

--- localRfstats/split_geno_0810.sh
+++ localRfstats/split_geno_0810.sh
@@ -1,10 +1,6 @@
 #!/mnt/hdd/hmiwa
 echo "start:`date`"
 #yanarhs_f3_analysis
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part1.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part1.u.txt
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part2.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part2.u.txt
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part3.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part3.u.txt
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part4.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part4.u.txt
 part1=$(< /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part1.u.txt)
 part2=$(< /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part2.u.txt)
 part3=$(< /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part3.u.txt)
@@ -14,10 +10,6 @@ cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats
 cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis.geno | cut -b $part3 > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part3.geno;echo "done"
 cat /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis.geno | cut -b $part4 > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yanarhs_f3_analysis_part4.geno;echo "done"
 #yana_j_f3_analysis
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part1.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part1.u.txt
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part2.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part2.u.txt
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part3.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part3.u.txt
-nkf -Lu /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part4.txt > /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part4.u.txt
 part1=$(< /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part1.u.txt)
 part2=$(< /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part2.u.txt)
 part3=$(< /mnt/hdd/hmiwa/m_20211215/07_yana_saami3/git/analysis_yanarhsf3/localRfstats/sources/yana_j_f3_analysis_part3.u.txt)

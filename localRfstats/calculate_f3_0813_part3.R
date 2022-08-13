f7c5ebde8afbc344d5e3e06f50767f45cf19d6b5
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-14 00:52:23 +0900

    [update] 2022/08/14

---
+++ localRfstats/calculate_f3_0813_part3.R
@@ -0,0 +1,147 @@
+#admixtools####
+.libPaths()
+.libPaths("C:\\Users\\himar\\Documents\\R")
+
+#install.packages("shiny")
+#install.packages("shinythemes")
+#install.packages("shinyjs")
+#install.packages("shinyBS")
+#install.packages("shinyFiles")
+#install.packages("shinyalert")
+#install.packages("shinyWidgets")
+#install.packages("shinydashboard")
+#install.packages("DT")
+#install.packages("jquerylib")
+library(admixtools)
+
+gc(reset=T)
+gc(reset=T)
+
+#dirctory setting####
+
+sdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\sources"
+rdir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3\\localRfstats\\results"
+ldir="G:\\hmiwa\\m_20211215\\07_yana_saami3\\git\\analysis_yanarhsf3"
+
+#analysis_part3####
+
+"yanarhs_f3_analysis_0813_part3"
+
+prefix = NULL
+my_f2_dir = NULL
+
+(prefix = sprintf("%s\\yanarhs_f3_analysis_altai_part3",sdir))
+(my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_0813_altai_part3",rdir))
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+f2_blocks = NULL
+dat.f3 = NULL
+
+f2_blocks = f2_from_precomp(my_f2_dir)
+dim(f2_blocks)
+inds <- read.table(sprintf("%s\\yanarhs_f3_analysis_altai_part3.ind",sdir),header=F)
+(lista <- unique(inds$V3))
+#(lista <- read.table(sprintf("%s\\lista_yana.txt",ldir))$V1)
+#(pop1 = as.character(lista[1]))
+(pop1 = as.character(lista[3]))
+#(pop2 = as.character(lista[9]))
+(pop2 = as.character(lista[2]))
+#(pop3 = as.character(lista[-1]))
+(pop3 = as.character(lista[-3]))
+dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+write.table(dat.f3,sprintf("%s\\yanarhs_f3_analysis_0813_altai_part3.f3_FIN.txt",rdir),quote=F,row.names=F)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+(prefix = sprintf("%s\\yanarhs_f3_analysis_chagyrskaya_part3",sdir))
+(my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_0813_chagyrskaya_part3",rdir))
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+f2_blocks = NULL
+dat.f3 = NULL
+
+f2_blocks = f2_from_precomp(my_f2_dir)
+dim(f2_blocks)
+inds <- read.table(sprintf("%s\\yanarhs_f3_analysis_chagyrskaya_part3.ind",sdir),header=F)
+(lista <- unique(inds$V3))
+#(lista <- read.table(sprintf("%s\\lista_yana.txt",ldir))$V1)
+#(pop1 = as.character(lista[1]))
+(pop1 = as.character(lista[3]))
+#(pop2 = as.character(lista[9]))
+(pop2 = as.character(lista[2]))
+#(pop3 = as.character(lista[-1]))
+(pop3 = as.character(lista[-3]))
+dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+write.table(dat.f3,sprintf("%s\\yanarhs_f3_analysis_0813_chagyrskaya_part3.f3_FIN.txt",rdir),quote=F,row.names=F)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+(prefix = sprintf("%s\\yanarhs_f3_analysis_denisova_part3",sdir))
+(my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_0813_denisova_part3",rdir))
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+f2_blocks = NULL
+dat.f3 = NULL
+
+f2_blocks = f2_from_precomp(my_f2_dir)
+inds <- read.table(sprintf("%s\\yanarhs_f3_analysis_denisova_part3.ind",sdir),header=F)
+(lista <- unique(inds$V3))
+#(lista <- read.table(sprintf("%s\\lista_yana.txt",ldir))$V1)
+#(pop1 = as.character(lista[1]))
+(pop1 = as.character(lista[3]))
+#(pop2 = as.character(lista[9]))
+(pop2 = as.character(lista[2]))
+#(pop3 = as.character(lista[-1]))
+(pop3 = as.character(lista[-3]))
+dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+write.table(dat.f3,sprintf("%s\\yanarhs_f3_analysis_0813_denisova_part3.f3_FIN.txt",rdir),quote=F,row.names=F)
+
+gc(reset=T)
+gc(reset=T)
+
+prefix = NULL
+my_f2_dir = NULL
+
+(prefix = sprintf("%s\\yanarhs_f3_analysis_vindija_part3",sdir))
+(my_f2_dir = sprintf("%s\\yanarhs_f3_analysis_0813_vindija_part3",rdir))
+extract_f2(prefix, my_f2_dir)
+
+gc(reset=T)
+gc(reset=T)
+
+f2_blocks = NULL
+dat.f3 = NULL
+
+f2_blocks = f2_from_precomp(my_f2_dir)
+dim(f2_blocks)
+inds <- read.table(sprintf("%s\\yanarhs_f3_analysis_vindija_part3.ind",sdir),header=F)
+(lista <- unique(inds$V3))
+#(lista <- read.table(sprintf("%s\\lista_yana.txt",ldir))$V1)
+#(pop1 = as.character(lista[1]))
+(pop1 = as.character(lista[3]))
+#(pop2 = as.character(lista[9]))
+(pop2 = as.character(lista[2]))
+#(pop3 = as.character(lista[-1]))
+(pop3 = as.character(lista[-3]))
+dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+write.table(dat.f3,sprintf("%s\\yanarhs_f3_analysis_0813_vindija_part3.f3_FIN.txt",rdir),quote=F,row.names=F)
+
+gc(reset=T)
+gc(reset=T)
